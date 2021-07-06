f86d4614e80327bc95ad819a06b44125b6adf0e0
Alix Fachin <alix.fachin@me.com>
2021-07-06 10:48:42 +0900

    Switching database to PostgresQL by default

--- myRecipe/settings.py
+++ myRecipe/settings.py
@@ -80,8 +80,16 @@ WSGI_APPLICATION = 'myRecipe.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DBNAME'),
+        'USER': config('DBUSER'),
+        'PASSWORD': config('DBPWD'),
+        'HOST': config('DBHOST'),
+        'PORT': '',
+    },
+    'test': {
+       'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
